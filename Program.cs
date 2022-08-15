a79ef79f664177a269d9b22e1c87ad8f51589239
Jp <jyotiprakashpc@outlook.com>
2022-08-15 11:54:09 +0530

    Welcome Code

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BinarySearchTree
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program.");
+        }
+    }
+}
